25d2943710c00332c3c4d8c81f80edaed9ce93c0
ErickC1103 <erickpatriciocardenas113003@gmail.com>
2025-12-11 11:08:35 -0500

    commit8

--- CS416_final/Accounts/forms.py
+++ CS416_final/Accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import WishlistItem
 
 class TicketForm(forms.ModelForm):
@@ -8,3 +10,17 @@ class TicketForm(forms.ModelForm):
         widgets = {
             'tickets': forms.NumberInput(attrs={'class': 'form-control', 'min': 1})
         }
+
+        class LoginForm(AuthenticationForm):
+            username = forms.CharField(
+                widget=forms.TextInput(attrs={
+                    "class": "form-control text-primary",
+                    "placeholder": "Username",
+                })
+            )
+            password = forms.CharField(
+                widget=forms.PasswordInput(attrs={
+                    "class": "form-control",
+                    "placeholder": "Password"
+                })
+            )
