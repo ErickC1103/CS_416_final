b7e09cf6e7c1330b78a07bc2a4936dd52fe66476
ErickC1103 <erickpatriciocardenas113003@gmail.com>
2025-12-11 01:48:49 -0500

    commit8

--- CS416_final/CS416_final/settings.py
+++ CS416_final/CS416_final/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-#STATICFILES_DIRS['static']
+#STATICFILES_DIRS = ['static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
